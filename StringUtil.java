d3fcdf91f81b83e3187cc26be37d9cf7e3138079
Michael Canavan <168924146+michaelc725@users.noreply.github.com>
2024-08-19 22:14:14 -0400

    Update StringUtil.java

--- StringUtil.java
+++ StringUtil.java
@@ -1,10 +1,13 @@
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
+import java.util.Base64;
 
-public class StringUtil {
+public class StringUtil
+{
     //Applies Sha256 to a string and returns the result
-    public static String applySha256(String input){
-        try {
+    public static String applySha256(String input)
+    {
+        try
+        {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             //Applies sha256 to out input
             byte[] hash = digest.digest(input.getBytes("UTF-8"));
@@ -16,9 +19,50 @@ public class StringUtil {
             }
             return hexString.toString();
         }
-        catch (Exception e) {
+        catch (Exception e)
+        {
             throw new RuntimeException(e);
         }
     }
 
+    // Applies ECDSA Signature and returns the result ( as bytes ).
+    public static byte[] applyECDSASig(PrivateKey privateKey, String input)
+    {
+        Signature dsa;
+        byte[] output = new byte[0];
+        try {
+            dsa = Signature.getInstance("ECDSA", "BC");
+            dsa.initSign(privateKey);
+            byte[] strByte = input.getBytes();
+            dsa.update(strByte);
+            byte[] realSig = dsa.sign();
+            output = realSig;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return output;
+
+    }
+
+    // Verifies a String signature
+    public static boolean verifyECDSASig(PublicKey publicKey, String data, byte[] signature)
+    {
+        try{
+            Signature ecdsaVerify = Signature.getInstance("ECDSA", "BC");
+            ecdsaVerify.initVerify(publicKey);
+            ecdsaVerify.update(data.getBytes());
+            return ecdsaVerify.verify(signature);
+        }
+        catch(Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    public static String getStringFromKey(Key key)
+    {
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
 }
