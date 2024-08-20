a8141a9a609dc67a3fb4af9ca47536de99a9d6dd
Michael Canavan <168924146+michaelc725@users.noreply.github.com>
2024-08-19 22:14:05 -0400

    added new classes

---
+++ Wallet.java
@@ -0,0 +1,38 @@
+
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
+
+public class Wallet
+{
+    public PrivateKey privateKey;
+    public PublicKey publicKey;
+
+    public Wallet()
+    {
+        generateKeyPair();
+    }
+
+    public void generateKeyPair()
+    {
+        try
+        {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA", "BC");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+            //Initialize they key generator and generate a KeyPair
+            keyGen.initialize(ecSpec, random); // 256 bytes provide an acceptable security level
+            KeyPair keyPair = keyGen.generateKeyPair();
+            // Set the public and private keys from the KeyPair
+            privateKey = keyPair.getPrivate();
+            publicKey = keyPair.getPublic();
+
+
+        }
+        catch(Exception e)
+        {
+            throw new RuntimeException(e);
+
+
+        }
+    }
+}
